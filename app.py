7dc2e8e6ee4b964ff7c6dea8c26de57b71fea629
Paulo Giavoni <paulo.giavoni@gmail.com>
2024-07-23 09:36:25 +0000

    primeiros passos

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="REVIT DASHBOARD", page_icon="bar_chart:", layout="wide")
+
+def app():
+    st.title("Hello, World!")
+    st.write("Bem-vindo ao meu primeiro site com Streamlit!")
+
+if __name__ == "__main__":
+    app()
